ae7a6a376df8a6afbc5549673ab0118bcfd46659
Sudhir Gaikwad <sudhir.gaikwad@softdel.com>
2025-10-06 10:20:00 +0530

    Updated main.py before deployment

--- main.py
+++ main.py
@@ -142,5 +142,8 @@ def submit_schedule():
         return jsonify({"success": False})
 
 
+#if __name__ == "__main__":
+   # app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
